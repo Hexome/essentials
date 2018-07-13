dcea7d22699282fc4dd04a76ff3b8f6e12033cf4
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2018-07-13 18:51:42 -0300

    Up update to database developments
    update install development database initials

--- 001.utils.sh
+++ 001.utils.sh
@@ -1,2 +1,12 @@
+# Development of database in the debian
+sudo apt install php*-sql* -y -f;
+sudo apt install php*-*sql -y -f;
+sudo apt install php*-common -y -f;
+sudo apt install php7*-common -y -f; 
+sudo apt install php*-common -y -f;    
+sudo a2enmod proxy_fcgi setenvif;
+sudo a2enconf php7.0-fpm;
+php --ini;
+
 # install dirmngr
-sudo apt-get install dirmngr
+sudo apt-get install dirmngr;
